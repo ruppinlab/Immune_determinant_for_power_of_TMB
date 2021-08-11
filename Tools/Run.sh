603a32f6df8b6eaaa186e71a4af458a0b769a044
NEELAM SINHA <Neelamsinha0592@gmail.com>
2021-08-11 01:07:52 -0400

    Run.sh

---
+++ Tools/Run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export RSTUDIO_PANDOC=/Applications/RStudio.app/Contents/MacOS/pandoc
+
+Rscript -e "rmarkdown::render('s0_Build_Immune_matrix.Rmd')"
+Rscript -e "rmarkdown::render('s1_Load_Preprocess_MSKCC.Rmd')"
+Rscript -e "rmarkdown::render('Figure_1_forest_plot_ICI_response.Rmd')"
+Rscript -e "rmarkdown::render('Figure2_and_sup_figure_2.Rmd')"
+Rscript -e "rmarkdown::render('Figure_3_mulivariate_model.Rmd')"
